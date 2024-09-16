5cbe8646857ef3f4eb4e7935a0e54cc766b21e98
Archana Bansal <archanabansal@Archanas-MacBook-Pro.local>
2024-09-16 11:07:25 -0700

    Adding code to the project

---
+++ src/components/stopwatch.js
@@ -0,0 +1,51 @@
+import { useState, useEffect,useRef } from "react";
+import "./stopwatch.css";
+
+const StopWatch = () =>{
+    const [time,setTime] = useState(0);
+    const [isRunning, setIsrunning] = useState(true);
+    const timer = useRef();
+
+    useEffect(()=>{
+        if(isRunning){
+            timer.current = setInterval(()=>{
+                setTime(prevTime => prevTime + 1);
+            },1000)
+        }
+        return ()=> clearInterval(timer.current);
+    },[isRunning])
+
+    const Format = (time) => {
+        let hours = Math.floor(time / 60 / 60 % 24);
+        let minutes = Math.floor(time /60 % 60);
+        let seconds = Math.floor(time % 60);
+
+        hours = hours < 10 ? "0"+ hours : hours;
+        minutes = minutes < 10 ? "0"+ minutes : minutes;
+        seconds = seconds < 10 ? "0"+ seconds : seconds;
+
+        return `${hours}:${minutes}:${seconds}`;
+    }
+
+    const handleStop = () =>{
+        if(isRunning){
+            clearInterval(timer.current);
+        }
+        setIsrunning(!isRunning);
+    }
+
+    return (
+        <div className="stopwatch">
+            <p className="timer">{Format(time)}</p>
+            <div className="actions">
+                <button className="restart-button" onClick={()=> setTime(0)}>Restart</button>
+                <button className="stop-button" onClick = {handleStop}>
+                    {isRunning ? "Stop" : "Resume"}
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default StopWatch;
